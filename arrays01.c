448d9b4b93a44114f849c75f0b4bc80cb7e930c1
Isha Negi <itsishanegi@gmail.com>
2024-09-15 19:50:48 +0530

    Add files via upload

---
+++ arrays01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define MAX 10
+
+void main()
+
+{ int  a [MAX]= {34,54,32,45,22};
+
+
+for(int i=0;i<MAX;i++)
+{
+    printf("the %d element of array: %d\n",i+1,a[i]);
+
+}
+
+}
